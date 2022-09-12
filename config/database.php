9a03d4a92253186a63dbe72ace3837e7b9b14743
Joel Gornati <jgornati@users.noreply.github.com>
2022-09-11 21:26:35 -0300

    added postgres database configuration

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
-
+$DATABASE_URL=parse_url('postgres://wumyttpmctswvn:9c6b1c642e45da57ef72f55b935ed1f9ad56c6da6a724cc39fd34a81daeafd0e@ec2-34-193-44-192.compute-1.amazonaws.com:5432/d4rjjcr9lvmk2t');
 return [
 
     /*
@@ -65,17 +65,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'schema' => 'public',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
